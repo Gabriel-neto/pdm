bb6c4cd2d10ee366f978d04ca6e7d17d27f14bc0
Gabriel.amado <gabrielamado702@gmail.com>
2023-08-29 21:18:32 -0300

    aula02

---
+++ aula02/componets/Box.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+import { View } from 'react-native';
+
+const Box = (props) => {
+  const [state, setState] = useState(false);
+
+  // estilo inline
+  const boxStyle = {
+    height: props.size,
+    width: props.size,
+    // flex: props.size,
+    backgroundColor: props.color,
+  };
+
+  return <View style={boxStyle}></View>;
+};
+
+export default Box;
---
+++ praticas/pratica01/tasks.js
@@ -0,0 +1,19 @@
+tasks [
+    {
+        "id":0,
+        "name":0,
+        "complet":0
+
+    }
+]
+
+function getTask() {
+    return tasks
+}
+
+function addTask(taskName){
+    const Ntaks[
+        
+    ]
+
+}
